142edbc88eb2ffb2b9a8b1f360b30297de3defef
MeShoSpam <45091970+MeShoSpam@users.noreply.github.com>
2018-11-19 12:24:02 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "5";
+var prefix = "1";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
@@ -32,14 +32,8 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
  
 client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
 client.login(process.env.BOT_TOKEN3);
 client.login(process.env.BOT_TOKEN4);
 client.login(process.env.BOT_TOKEN5);
 client.login(process.env.BOT_TOKEN6);
-client.login(process.env.BOT_TOKEN7);
-client.login(process.env.BOT_TOKEN8);
-client.login(process.env.BOT_TOKEN9);
-client.login(process.env.BOT_TOKEN10);
-client.login(process.env.BOT_TOKEN11);
-client.login(process.env.BOT_TOKEN12);
-client.login(process.env.BOT_TOKEN13);
